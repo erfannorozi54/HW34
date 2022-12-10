4a6828415feb415b72ed97f2e3aa610b31cefa1f
Erfan <erfan.norozi54@gmail.com>
2022-12-10 19:15:07 +0330

    starting project

---
+++ CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/crypto.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/message.cpp.o"
+  "CMakeFiles/main.dir/src/server.cpp.o"
+  "CMakeFiles/main.dir/src/unit_test.cpp.o"
+  "CMakeFiles/main.dir/src/user.cpp.o"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
